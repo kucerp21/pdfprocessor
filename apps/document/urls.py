72d8e0798bf7eea738e912716bcbfb9fb5042a3f
Petr Kucera <petr.kucera@twisto.cz>
2021-03-01 10:17:11 +0100

    Add endpoint to view normalized images

--- apps/document/urls.py
+++ apps/document/urls.py
@@ -1,13 +1,12 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from .views import PDFPageViewSet, PDFDocumentViewSet
+from .views import PDFDocumentViewSet
 
 app_name = 'document'
 
 router = routers.DefaultRouter()
 router.register(r'document', PDFDocumentViewSet)
-# router.register(r'document/<document_id>/page/<page_number>', PDFPageViewSet)
 
 urlpatterns = [
     path('', include(router.urls))
